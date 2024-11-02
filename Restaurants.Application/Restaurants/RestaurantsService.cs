fb37d6a3bff0c840f09334d1e530f96892e24b18
arshadahamed <arshadpayoneer@gmail.com>
2024-11-02 20:08:08 +0530

    Returing Dtos

--- Restaurants.Application/Restaurants/RestaurantsService.cs
+++ Restaurants.Application/Restaurants/RestaurantsService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Restaurants.Application.Restaurants.Dtos;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Respositories;
 
@@ -7,17 +8,21 @@ namespace Restaurants.Application.Restaurants;
 internal class RestaurantsService(IRestaurantsRepository restaurantsRepository,
     ILogger<RestaurantsService> logger) : IRestaurantsService
 {
-    public async Task<IEnumerable<Restaurant>> GetAllRestaurants()
+    public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
     {
         logger.LogInformation("Getting All Restaurants");
         var restaurants = await restaurantsRepository.GetAllAsync();
-        return restaurants;
+
+        var restaurantsDtos = restaurants.Select(RestaurantDto.FromEntity);
+
+        return restaurantsDtos!;
     }
 
-    public async Task<Restaurant?> GetById(int id)
+    public async Task<RestaurantDto?> GetById(int id)
     {
         logger.LogInformation($"Getting Restaurant {id}");
         var restaurant = await restaurantsRepository.GetByIdAsync(id);
-        return restaurant;
+        var restaurantDto = RestaurantDto.FromEntity(restaurant);
+        return restaurantDto;
     }
 }
--- Restaurants.Domain/Entities/Restaurant.cs
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -1,4 +1,4 @@
-﻿using System.Net.Sockets;
+﻿ using System.Net.Sockets;
 
 namespace Restaurants.Domain.Entities;
 
