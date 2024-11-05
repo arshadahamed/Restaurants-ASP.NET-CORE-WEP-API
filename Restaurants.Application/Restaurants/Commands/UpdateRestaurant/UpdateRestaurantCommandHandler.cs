58b7cd2d097d08474abf4b914e4d7097d55cfead
arshadahamed <arshadpayoneer@gmail.com>
2024-11-05 15:56:02 +0530

    Added Custom Exceptions like NotFoundException

--- Restaurants.Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/UpdateRestaurant/UpdateRestaurantCommandHandler.cs
@@ -2,27 +2,27 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Restaurants.Application.Restaurants.Commands.DeleteRestaurant;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
 using Restaurants.Domain.Respositories;
 
 namespace Restaurants.Application.Restaurants.Commands.UpdateRestaurant;
 
 public class UpdateRestaurantCommandHandler(ILogger<UpdateRestaurantCommandHandler> logger,
-    IRestaurantsRepository restaurantsRepository, IMapper mapper) : IRequestHandler<UpdateRestaurantCommand, bool>
+    IRestaurantsRepository restaurantsRepository, IMapper mapper) : IRequestHandler<UpdateRestaurantCommand>
 {
-    public async Task<bool> Handle(UpdateRestaurantCommand request, CancellationToken cancellationToken)
+    public async Task Handle(UpdateRestaurantCommand request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Updating Restaurant with id : {RestaurantId} with {@UpdatedRestaurant}", request.Id, request);
         var restaurant = await restaurantsRepository.GetByIdAsync(request.Id);
         if (restaurant is null)
-            return false;
-        
+            throw new NotFoundException(nameof(Restaurant), request.Id.ToString());
+
         mapper.Map(request, restaurant);
         //restaurant.Name = request.Name;
         //restaurant.Description = request.Description;
         //restaurant.HasDelivery = request.HasDelivery;
 
         await restaurantsRepository.Update();
-
-        return true;
     }
 }
--- Restaurants.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQueryHandler.cs
+++ Restaurants.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQueryHandler.cs
@@ -1,20 +1,23 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
 using Restaurants.Domain.Respositories;
 
 namespace Restaurants.Application.Restaurants.Queries.GetRestaurantById;
 
 public class GetRestaurantByIdQueryHandler (ILogger<GetRestaurantByIdQueryHandler> logger, IMapper mapper, IRestaurantsRepository restaurantsRepository) 
-    : IRequestHandler<GetRestaurantByIdQuery, RestaurantDto?>
+    : IRequestHandler<GetRestaurantByIdQuery, RestaurantDto>
 {
-    public async Task<RestaurantDto?> Handle(GetRestaurantByIdQuery request, CancellationToken cancellationToken)
+    public async Task<RestaurantDto> Handle(GetRestaurantByIdQuery request, CancellationToken cancellationToken)
     {
         logger.LogInformation("Getting Restaurant {RestaurantId}", request.Id);
-        var restaurant = await restaurantsRepository.GetByIdAsync(request.Id);
-        var restaurantDto = mapper.Map<RestaurantDto?>(restaurant);
+        var restaurant = await restaurantsRepository.GetByIdAsync(request.Id)
+            ?? throw new NotFoundException(nameof(Restaurant), request.Id.ToString());
+
+        var restaurantDto = mapper.Map<RestaurantDto>(restaurant);
 
         return restaurantDto;
     } 
