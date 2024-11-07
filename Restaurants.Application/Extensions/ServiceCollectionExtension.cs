4d2cbe666cff2019bcedeca3d0199adcf2cdbad0
arshadahamed <arshadpayoneer@gmail.com>
2024-11-07 22:42:49 +0530

    User Context and Interface created

--- Restaurants.Application/Extensions/ServiceCollectionExtension.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtension.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
+using Restaurants.Application.User;
 
 namespace Restaurants.Application.Extensions;
 
@@ -13,5 +14,8 @@ public static class ServiceCollectionExtensions
         services.AddAutoMapper(applicationAssemby);
         services.AddValidatorsFromAssembly(applicationAssemby)
             .AddFluentValidationAutoValidation();
+
+        services.AddScoped<IUserContext, UserContext>();
+        services.AddHttpContextAccessor();
     }
 }
