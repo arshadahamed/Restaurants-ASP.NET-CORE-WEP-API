84a76cae7b43b52ca836700f8b88bec67bf972f9
arshadahamed <arshadpayoneer@gmail.com>
2024-11-07 19:21:01 +0530

    Extracting presentation layer services

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,51 +1,21 @@
-using Microsoft.Extensions.Configuration;
-using Microsoft.OpenApi.Models;
 using Restaurants.API.Middlewares;
 using Restaurants.Application.Extensions;
 using Restaurants.Domain.Entities;
 using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
+using Restaurants.API.Extensions;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen(configuration =>
-{
-    configuration.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer"
-    });
 
-    configuration.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference {Type= ReferenceType.SecurityScheme, Id = "bearerAuth"}
-            },
-            []
-        }
-        
-    });
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddScoped<ErrorHandlingMiddleware>();
-builder.Services.AddScoped<RequestTimeLoggingMiddleware>();
-
-
+builder.AddPresentation();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Host.UseSerilog((context, configuration) =>
-    configuration
-        .ReadFrom.Configuration(context.Configuration)
-);
+
 
 var app = builder.Build();
 
