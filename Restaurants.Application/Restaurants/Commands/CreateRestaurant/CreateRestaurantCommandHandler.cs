497755669e0eecafd1aad9c47c8c4ce8b744c886
arshadahamed <arshadpayoneer@gmail.com>
2024-11-09 13:21:19 +0530

    Created User resources with Restaureant

--- Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
+++ Restaurants.Application/Restaurants/Commands/CreateRestaurant/CreateRestaurantCommandHandler.cs
@@ -1,19 +1,29 @@
 ﻿using AutoMapper;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using Restaurants.Application.Users;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Respositories;
 
 namespace Restaurants.Application.Restaurants.Commands.CreateRestaurant;
 
-public class CreateRestaurantCommandHandler(ILogger<CreateRestaurantCommandHandler> logger,IMapper mapper,IRestaurantsRepository restaurantsRepository) 
+public class CreateRestaurantCommandHandler(ILogger<CreateRestaurantCommandHandler> logger,
+    IMapper mapper,
+    IRestaurantsRepository restaurantsRepository,
+    IUserContext userContext) 
     : IRequestHandler<CreateRestaurantCommand, int>
 {
     public async Task<int> Handle(CreateRestaurantCommand request, CancellationToken cancellationToken)
     {
-        logger.LogInformation("Creating a new Restaurant {@Restaurant}", request);
+        var currentUser = userContext.GetCurrentUser();
+
+        logger.LogInformation("{UserName} [{UserId}] Creating a new Restaurant {@Restaurant}",
+            currentUser.Email,
+            currentUser.Id,
+            request);
 
         var restaurant = mapper.Map<Restaurant>(request);
+        restaurant.OwnerId = currentUser.Id;
 
         int id = await restaurantsRepository.Create(restaurant);
         return id;
--- Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -23,6 +23,12 @@ internal class RestaurantsDbContext(DbContextOptions<RestaurantsDbContext> optio
             .WithOne()
             .HasForeignKey(d => d.RestaurantId);
 
+        modelBuilder.Entity<User>()
+            .HasMany(o => o.OwnedRestaurants)
+            .WithOne(r => r.Owner)
+            .HasForeignKey(r => r.OwnerId);
+            
+
     }
 
 }
